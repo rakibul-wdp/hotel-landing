7377d637febe6089497a069ee3d022d4b5952df6
rakibul-wdp <rakibul.wdp@gmail.com>
2023-01-04 21:19:24 +0600

    add footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import HotelDetails from "./components/HotelDetails";
 import Navbar from "./components/Navbar";
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <HotelDetails />
       <div className="divider px-[10rem]"></div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="px-[14.5rem] mb-28">
+      <h3 className="text-lg font-bold">Property Rules</h3>
+      <p className="mb-1">Check-in: 12pm Check-out: 12pm</p>
+      <div className="flex justify-between px-8">
+        <ul className="list-disc">
+          <li>Guest with fester are not allowed</li>
+          <li>Guest from comainment comes are not allowed</li>
+          <li>Guest takes 10 year of age are not allowed al this property.</li>
+          <li>PAN Card and Non-Govt IDs are not acceepted as ID pthot(a)</li>
+          <li>
+            Aadhar , Driving listense and Govt. ID are accepted at ID proof(a)
+          </li>
+          <li>Propety staff is tranfeed on hygineve guidenses</li>
+          <li>Proff are not allowed.</li>
+          <li>Outside food is not allowed</li>
+          <li>Smoking within the permission is not allowed</li>
+          <li>Smoking within the permission in not allowed</li>
+          <li>Does not allow private parfies of events</li>
+          <li>
+            Quarantine process are being followed as per local government
+            authories
+          </li>
+        </ul>
+        <ul className="list-disc">
+          <li>Pert are not allowed</li>
+          <li>Outside food is not allowed</li>
+          <li>Smoking within the permission is not allowed</li>
+          <li>Doew not allow private aprties of events</li>
+          <li>
+            Quarniten process are being followed as per local governemtn need
+            permission.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
